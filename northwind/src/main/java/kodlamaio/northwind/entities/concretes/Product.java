9e6c559f37497b37cdd14a54ba5989be8efbc394
Hasan Erdem AK <70165677+hasanerdemak@users.noreply.github.com>
2023-09-29 12:24:22 +0300

    day 6 codes added

---
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -0,0 +1,34 @@
+package kodlamaio.northwind.entities.concretes;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="products")
+@Data
+public class Product {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="product_id")
+	private int id;
+	
+	@Column(name="category_id")
+	private int categoryId;
+	
+	@Column(name="product_name")
+	private String productName;
+	
+	@Column(name="unit_price")
+	private double unitPrice;
+	
+	@Column(name="units_in_stock")
+	private short unitsInStock;
+	
+	@Column(name="quantity_per_unit")
+	private String quantityPerUnit;
+}
