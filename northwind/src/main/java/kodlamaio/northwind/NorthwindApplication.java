b5c51b930490d43d3f1bc2bf5ba3f61032951c73
Hasan Erdem AK <70165677+hasanerdemak@users.noreply.github.com>
2023-10-04 01:29:28 +0300

    Day 7 codes added

--- northwind/src/main/java/kodlamaio/northwind/NorthwindApplication.java
+++ northwind/src/main/java/kodlamaio/northwind/NorthwindApplication.java
@@ -3,7 +3,23 @@ package kodlamaio.northwind;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Northwind",
+				version = "1.0.0",
+				description = "Northwind project for Java & React Camp",
+				contact = @Contact(
+						name = "Hasan Erdem Ak",
+						email = "hasanerdemak@gmail.com"
+				)
+			
+		)
+)
 public class NorthwindApplication {
 
 	public static void main(String[] args) {
--- northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -3,17 +3,22 @@ package kodlamaio.northwind.entities.concretes;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Data;
 
+@Data
 @Entity
 @Table(name="products")
-@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="product_id")
 	private int id;
 	
