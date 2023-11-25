d1ef4e17298772ffa860bb8e4bafe48e74de2853
Hasan Erdem AK <70165677+hasanerdemak@users.noreply.github.com>
2023-11-25 23:03:22 +0300

    Day 8 codes added

--- northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
+++ northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -3,6 +3,9 @@ package kodlamaio.northwind.business.concretes;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
@@ -14,28 +17,79 @@ import kodlamaio.northwind.dataAccess.abstracts.ProductDao;
 import kodlamaio.northwind.entities.concretes.Product;
 
 @Service
-public class ProductManager implements ProductService{
-	
+public class ProductManager implements ProductService {
+
 	private ProductDao productDao;
-	
+
 	@Autowired
 	public ProductManager(ProductDao productDao) {
 		super();
 		this.productDao = productDao;
 	}
 
-
 	@Override
 	public DataResult<List<Product>> getAll() {
-		return new SuccessDataResult<List<Product>>
-		(this.productDao.findAll(), "Data listed");
+		return new SuccessDataResult<List<Product>>(this.productDao.findAll(), "Data listed");
 	}
-
 
 	@Override
 	public Result add(Product product) {
 		this.productDao.save(product);
 		return new SuccessResult("Product added");
+	}
+
+	@Override
+	public DataResult<Product> getByProductName(String productName) {
+		return new SuccessDataResult<Product>(this.productDao.getByProductName(productName), "Data listed");
+	}
+
+	@Override
+	public DataResult<Product> getByProductNameAndCategoryId(String productName, int categoryId) {
+		return new SuccessDataResult<Product>(this.productDao.getByProductNameAndCategory_CategoryId(productName, categoryId),
+				"Data listed");
+	}
+
+	@Override
+	public DataResult<List<Product>> getByProductNameOrCategoryId(String productName, int categoryId) {
+		return new SuccessDataResult<List<Product>>(this.productDao.getByProductNameOrCategory_CategoryId(productName, categoryId),
+				"Data listed");
+	}
+
+	@Override
+	public DataResult<List<Product>> getByCategoryIn(List<Integer> categories) {
+		return new SuccessDataResult<List<Product>>(this.productDao.getByCategoryIn(categories), "Data listed");
+	}
+
+	@Override
+	public DataResult<List<Product>> getByProductNameContains(String productName) {
+		return new SuccessDataResult<List<Product>>(this.productDao.getByProductNameContains(productName),
+				"Data listed");
+	}
+
+	@Override
+	public DataResult<List<Product>> getByProductNameStartsWith(String productName) {
+		return new SuccessDataResult<List<Product>>(this.productDao.getByProductNameStartsWith(productName),
+				"Data listed");
+	}
+
+	@Override
+	public DataResult<List<Product>> getByNameAndCategory(String productName, int categoryId) {
+		return new SuccessDataResult<List<Product>>(this.productDao.getByNameAndCategory(productName, categoryId),
+				"Data listed");
+	}
+
+	@Override
+	public DataResult<List<Product>> getAll(int pageNo, int pageSize) {
+		// page no 0'dan başladığı için 1 çıkardık.
+		Pageable pageable = PageRequest.of(pageNo-1, pageSize);
+		
+		return new SuccessDataResult<List<Product>>(this.productDao.findAll(pageable).getContent(), "Data listed");
+	}
+
+	@Override
+	public DataResult<List<Product>> getAllSorted() {
+		Sort sort = Sort.by(Sort.Direction.DESC, "productName"); 
+		return new SuccessDataResult<List<Product>>(this.productDao.findAll(sort), "Data listed");
 	}
 
 }
--- northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -22,8 +24,8 @@ public class Product {
 	@Column(name="product_id")
 	private int id;
 	
-	@Column(name="category_id")
-	private int categoryId;
+//	@Column(name="category_id")
+//	private int categoryId;
 	
 	@Column(name="product_name")
 	private String productName;
@@ -36,4 +38,10 @@ public class Product {
 	
 	@Column(name="quantity_per_unit")
 	private String quantityPerUnit;
+	
+	// Burada, bu Product'ın category'si nedir diye tuttuğumuz için
+	// yukarıdaki catedoryId'ye gerek yoktur. EF'de koyulabilir
+	@ManyToOne()
+	@JoinColumn(name="category_id")
+	private Category category;
 }
